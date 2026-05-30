74f9c035d6b3124df61d709330288b0664eae1fc
MatheusLessa <matheus.lessa.dev@gmail.com>
2026-05-29 22:10:31 -0300

    test: adiciona teste de integracao para listagem de tarefas3

---
+++ src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
@@ -0,0 +1,25 @@
+package br.com.faculdadedonaduzzi.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllTasks() throws Exception {
+        // ERRO INTENCIONAL: chamando /tarefa em vez de /tarefas
+        mockMvc.perform(get("/tarefa"))
+               .andExpect(status().isOk());
+    }
+}
