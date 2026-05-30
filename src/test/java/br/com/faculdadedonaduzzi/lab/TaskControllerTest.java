8b4decf68b9fae36add19dc29537599bb55ea966
MatheusLessa <matheus.lessa.dev@gmail.com>
2026-05-29 22:14:53 -0300

    fix: adiciona dependencia de teste e corrige import

--- src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
+++ src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
@@ -2,23 +2,24 @@ package br.com.faculdadedonaduzzi.lab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TaskControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    public void testGetAllTasks() {
+    public void testGetAllTasks() throws Exception {
         // ERRO INTENCIONAL: /tarefa em vez de /tarefas
-        ResponseEntity<String> response = restTemplate.getForEntity("/tarefa", String.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        mockMvc.perform(get("/tarefa"))
+               .andExpect(status().isOk());
     }
 }
